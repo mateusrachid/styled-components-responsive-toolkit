703785754d06b5cdd9f10976c06974fae140eda4
Mateus Rachid <mateus_rachid@outlook.com>
2021-06-04 00:30:18 -0300

    .

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const Head = require("./src/Head");
-const mediaQueryPlugin = require("./src/media-query-plugin");
-const { pxToRem, pxToRemPlugin } = require("./src/px-to-rem");
-const pxToViewportPlugin = require("./src/px-to-viewport-plugin");
+// const Head = require("./src/Head");
+// const mediaQueryPlugin = require("./src/media-query-plugin");
+// const { pxToRem, pxToRemPlugin } = require("./src/px-to-rem");
+// const pxToViewportPlugin = require("./src/px-to-viewport-plugin");
 
-const plugins = [
-  mediaQueryPlugin,
-  pxToViewportPlugin,
-  pxToRemPlugin,
-];
+// const plugins = [
+//   mediaQueryPlugin,
+//   pxToViewportPlugin,
+//   pxToRemPlugin,
+// ];
 
-module.exports = {
-  plugins,
-  pxToRem,
-  Head,
-};
+// module.exports = {
+//   plugins,
+//   pxToRem,
+//   Head,
+// };
+
+export const Head = ()=>null;
+
